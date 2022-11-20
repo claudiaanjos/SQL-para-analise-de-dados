e5ce4cc0207a11675ca63fcb87dbb27e60854e1a
Claudia dos Anjos <84209593+claudiaanjos@users.noreply.github.com>
2022-11-20 13:01:49 -0300

    Comandos de Seleção

--- Prática/comandos_de_selecao.sql
+++ Prática/comandos_de_selecao.sql
@@ -16,5 +16,22 @@ select
   total_price
 from e_commerce.items
 limit 3;
-  
+
+select distinct
+  status
+from e_commerce.orders;
+
+select distinct
+  state
+from e_commerce.customers;
+
+select distinct
+  first_name
+from `e_commerce.customers`;
+
+select distinct
+  extract(year from created_at) as ano
+from e_commerce.orders;
+
+
   
