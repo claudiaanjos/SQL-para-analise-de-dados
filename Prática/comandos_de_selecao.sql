b18f6d5d137b7f63bd45dcb456c6a5c31eea9e4f
Claudia dos Anjos <84209593+claudiaanjos@users.noreply.github.com>
2022-11-20 12:42:52 -0300

    Comandos de Selecção

---
+++ Prática/comandos_de_selecao.sql
@@ -0,0 +1,20 @@
+select 
+  1,
+  2,
+  'Olá!',
+  "Oi!",
+  1+8;
+
+select
+  id,
+  created_at
+from e_commerce.customers;
+
+select
+  order_id,
+  quantity,
+  total_price
+from e_commerce.items
+limit 3;
+  
+  
