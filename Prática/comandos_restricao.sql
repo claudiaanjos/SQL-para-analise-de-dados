5ad656dae82dcb3123e7f1a2e651a00420eecb96
Claudia dos Anjos <84209593+claudiaanjos@users.noreply.github.com>
2022-11-21 17:40:49 -0300

    Comandos de Restrição

--- Prática/comandos_restricao.sql
+++ Prática/comandos_restricao.sql
@@ -5,3 +5,23 @@ select
   *
 from e_commerce.items 
 where quantity > 4;
+
+select
+  *
+from e_commerce.items 
+where quantity = 5;
+
+select
+  *
+from e_commerce.items 
+where quantity between 2 and 4;
+
+select
+  *
+from e_commerce.customers 
+where state = 'Acre' and email is not null;
+
+select
+  *
+from e_commerce.customers 
+where state = 'Acre' or state = 'São Paulo' and street is not null;
