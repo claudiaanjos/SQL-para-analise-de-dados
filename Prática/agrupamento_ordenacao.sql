dd83f502398840d2fc5765efdf99d5c40d40c4a9
Claudia dos Anjos <84209593+claudiaanjos@users.noreply.github.com>
2022-11-22 16:58:12 -0300

    Agrupamento e Ordenação

--- Prática/agrupamento_ordenacao.sql
+++ Prática/agrupamento_ordenacao.sql
@@ -27,3 +27,32 @@ select
   order_id as pedido
 from e_commerce.items 
 group by 2;
+
+# Order by
+select
+  *
+from e_commerce.categories 
+order by id;
+
+select
+  *
+from e_commerce.categories 
+order by id desc;
+
+select
+  *
+from e_commerce.orders 
+order by customer_id asc, status desc;
+
+select
+  customer_id
+from e_commerce.orders 
+group by customer_id
+order by 1 desc;
+
+select
+  date(created_at) as dia,
+  count(id) as quantidade_pedidos
+from e_commerce.orders 
+group by dia
+order by quantidade_pedidos desc;
