49e6c2f0ad7a50977e6bc2c76c8d1f7b448f308b
Claudia dos Anjos <84209593+claudiaanjos@users.noreply.github.com>
2022-11-22 09:53:56 -0300

    Comandos Condicionais

--- Prática/comandos_condicionais.sql
+++ Prática/comandos_condicionais.sql
@@ -3,3 +3,32 @@ select
   name,
   case when name = 'Moda e Acessórios' then 'Sim, é moda e acessórios' end as caso1
 from e_commerce.categories;
+
+select
+  id,
+  name,
+  case 
+    when name = 'Moda e Acessórios' then 'Sim, é moda e acessórios' 
+    when name = 'Livros' then 'Sim, é livro' 
+    else 'Não encontrado' 
+  end as caso1
+from e_commerce.categories;
+
+# Retorna a ação do Primeiro Verdadeiro
+select
+  id,
+  case
+    when id between 0 and 5 then 'Primeiro Verdadeiro'
+    when id between 3 and 5 then 'Segundo Verdadeiro'
+  end as caso2,
+  name
+from e_commerce.categories;
+
+select distinct
+  state,
+  case
+    when state in ('São Paulo', 'Rio de Janeiro', 'Minas Gerais') then 'Sudeste'
+    else 'Não encontrado'
+  end as regiao
+from e_commerce.customers 
+where state is not null;
